49773a2b1e47806cbc73b67c3978efdab305a102
Rodrigo <rodrigomartins.agro@gmail.com>
2023-02-13 20:07:08 -0300

    feat: add req01

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const products = require('./routes/products');
+const { productRouter } = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use('/products', productRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.use('/products', products);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
--- src/routes/products.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (_req, res) => { });
-
-module.exports = router;
